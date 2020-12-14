d6a0e9ea1a1d301993a483e5bda5944d8a0f039f
MoPaMo <mpm1301@web.de>
2020-12-14 21:43:24 +0100

    covert .json to .js

--- trains.js
+++ trains.js
@@ -1,6 +1,11 @@
-{
+var x ={
     "trains":[
+        {"name":"Meditation" , "info":"Bringe Ruhe in dein Handeln, werde dir deiner bewusst und stärke Geist und Seele mit dieser Meditationsübung"},
         {"name":"Baum", "info":"Atme ein und strecke deine Arme nach oben. Mach deinen Körper lang und wachse wie ein Baum, bleibe dabei aber grade und gewurzelt. "},
         {"info":"Beide Hände vor den Brustkorb nehmen und ausatmenEinatmen, dabei beide Arme nach oben strecken, Schultern und Schulterblätter zusammenführen, Po anspannen (siehe Bild)Ausatmen, Oberkörper nach unten beugen und mit den Händen den Boden berührenEinatmen, dabei das linke Bein nach hinten strecken und den Fuß aufsetzen. Das rechte Bein strecken, Oberkörper aufrichtenAtem anhalten und auch das zweite Bein nach hinten stellen, die Arme sind durchgedrücktAusatmen, dabei Stirn und Brust den Boden berühren lassenEinatmen, dabei den Oberkörper anhebenAusatmen und mit dem Becken hochkommen, die Ferse auf den Boden pressen, die Arme sind durchgedrücktEinatmen, dabei rechten Fuß nach vorne stellen, Arme vom Boden lösen und nach hinten streckenAusatmen und dabei auch mit dem linken Fuß nach vorne kommen, die Beine durchstrecken, die Hände auf dem BodenEinatmen, beide Arme über den Kopf streckenAusatmen und beide Arme zur Hüfte führen Sonnengruß wiederholen, dabei zuerst das rechte Bein nach hinten strecken. Übung insgesamt dreimal absolvieren, um die Muskeln aufzuwärmen."}
+    ]
+    , 
+    traindates:[
+        {"excersises":["Mediation"], "name":"Einführung"}
     ]
 }
