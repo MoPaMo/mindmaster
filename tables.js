bdd94f70a78b8acdab49798884c9675995b37568
MoPaMo <mpm1301@web.de>
2020-12-17 16:04:01 +0100

    added time starting

---
+++ tables.js
@@ -0,0 +1,6 @@
+$(function(){
+    for (item of x.traindates){
+        $("#tables").append('<div class="p-1 sm:p-2 border-2 border-blue-200 h-10 sm:h-20 rounded">'+item.name+'</div>')
+        
+    }
+})
