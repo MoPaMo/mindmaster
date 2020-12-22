88e4a148a601e4d0d6eefada711e48777135dc41
MoPaMo <mpm1301@web.de>
2020-12-22 11:45:10 +0100

    added question page

--- tables.js
+++ tables.js
@@ -1,11 +1,35 @@
-$(function(){
-    for (item of x.traindates){
-        
-        $("#tables").append(`<div class="p-1 sm:p-2 border-2 border-blue-200  rounded" ><h3 class="">${item.name}</h3><div class="text-sm">${item.excersises.join(", ")}</div></div>`)
-        $("#tables").children().last().css({
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" height=\"50px\" width=\"120px\"><text x=\"100\" y=\"15\" fill=\"blue\" font-family=\"ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Roboto, Arial, sans-serif\" font-size=\"20\">${x.traindates.indexOf(item)+1}</text ><text x=\"101\" y=\"16\" fill=\"skyblue\" font-family=\"ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Roboto, Arial, sans-serif\" font-size=\"20\">${x.traindates.indexOf(item)+1}</text ></svg >')`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "top right"
-        })
+$(function () {
+  for (item of x.traindates) {
+    let copytrain = item.excersises;
+    for (exc of copytrain) {
+      copytrain[
+        copytrain.indexOf(exc)
+      ] = `<svg version="1.1" viewBox="0 0 75 50" xmlns="http://www.w3.org/2000/svg"
+            xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/"
+            xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" class="inline-block w-6 rounded-full" alt="${
+              gti(exc).name
+            }">${gti(exc).svg}</svg>`;
     }
-})
+    $("#tables").append(
+      `<div class="p-1 sm:p-2  rounded" ><h3 class="">${
+        item.name
+      }</h3><div class="text-sm">${
+        item.add
+      }</div><div class="sm:h-8 h-6 flex flex-row overflow-x-auto p-1">${copytrain.join(
+        ""
+      )}</div></div>`
+    );
+    $("#tables")
+      .children()
+      .last()
+      .css({
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" height=\"50px\" width=\"120px\"><text x=\"100\" y=\"15\" fill=\"blue\" font-family=\"ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Roboto, Arial, sans-serif\" font-size=\"20\">${
+          x.traindates.indexOf(item) + 1
+        }</text ><text x=\"101\" y=\"16\" fill=\"skyblue\" font-family=\"ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Roboto, Arial, sans-serif\" font-size=\"20\">${
+          x.traindates.indexOf(item) + 1
+        }</text ></svg >')`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top right",
+      });
+  }
+});
